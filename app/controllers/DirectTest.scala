a2d7813e07fd8265b0cbfb122f93a6975dc82717
Roy van Kaathoven <royvankaathoven@hotmail.com>
2013-05-23 23:13:37 +0200

    first version

---
+++ app/controllers/DirectTest.scala
@@ -0,0 +1,39 @@
+package controllers
+
+import com.scalext.annotations.Remotable
+import com.scalext.annotations.FormHandler
+
+@Remotable(name = "play.remotablectrl")
+class DirectTest {
+
+  private var itemList = List[String]()
+
+  @Remotable
+  def add(item: String): String = {
+    itemList ::= item
+    "added"
+  }
+
+  @Remotable
+  def additem2(item: String): String = {
+    itemList ::= item
+    "added"
+  }
+
+  @Remotable
+  def all(): List[String] = itemList
+
+  @Remotable(name = "list")
+  protected def list: List[String] = {
+    itemList
+  }
+}
+
+@Remotable
+class Direct2Test {
+
+  @Remotable(name = "gas2")
+  def AddItem(item: String) = {
+    println(s"Added Item $item")
+  }
+}
---
+++ project/Build.scala
@@ -0,0 +1,21 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "scalextexample"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    "commons-io" % "commons-io" % "2.1",
+    "org.apache.commons" % "commons-lang3" % "3.1",
+    "com.google.code.gson" % "gson" % "2.2.3"
+  )
+
+  val scalext = Project("scalext", file("modules/scalext"))
+
+  val scalextExample = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here      
+  ).dependsOn(scalext)
+}
